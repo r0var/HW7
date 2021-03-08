f5d5ededf54234fdd90f4262a2711d2fcbef0f24
Yauhen Ashkinadze <r0var@outlook.com>
2021-03-08 19:00:17 +0000

    Finish other fnctions

--- netscript.sh
+++ netscript.sh
@@ -29,12 +29,27 @@ function allLowercase {
   echo ${ARG2,,}
 }
 
+function arr {
+  array=($(grep SENT variables_file | sed s'/SENT=//' | sed s'/\"//g'))
+  echo ${array[0]}
+  local COUNT=${#array[*]}
+  echo ${array[$COUNT-1]}
+}
+
+function rewriteRecord {
+  local CHECK=$(grep "SENT" variables_file)
+  sed -i "s/$CHECK/SENT=\"$ARG2\"/" variables_file
+  echo "Entry changed!"
+}
+
 function printHelp {
   echo -e "   --help                            Shows help \n \
   --list                            Print servers addresses from servers_list \n \
   --work                            Executes commands from variables_file for servers in servers_list \n \
   --up [PARAMETER]                  Converts input string to all uppercase \n \
-  --low [PARAMETER]                 Converts input string to all lowercase"
+  --low [PARAMETER]                 Converts input string to all lowercase \n \
+  --arr                             Print array from special variable
+   --rw [PARAMETER]                  Replace array variable"
 }
 
 case $1 in
@@ -43,5 +58,7 @@ case $1 in
   --work) listWorker;;
   --up) allUppercase;;
   --low) allLowercase;;
+  --arr) arr;;
+  --rw) rewriteRecord;;
   *) echo "Try './netscript.sh --help' for more information.";;
 esac
